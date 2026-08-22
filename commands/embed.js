dcf9c895b04114d3af27b8a6eb80db716771c833
Salo <eyeshernandezthiago@gmail.com>
2026-08-21 19:44:10 -0500

    Add adminOnly property to embed command

--- commands/embed.js
+++ commands/embed.js
@@ -6,6 +6,7 @@ const {
 } = require('discord.js');
 
 module.exports = {
+    adminOnly: true,
     data: new SlashCommandBuilder()
         .setName('embed')
         .setDescription('Crea un embed personalizado'),
