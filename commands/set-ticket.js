40de9dd7b7758f2200c98a6de97f43e70ba77b76
Salo <eyeshernandezthiago@gmail.com>
2026-09-08 19:32:48 -0500

    Update set-ticket.js

--- commands/set-ticket.js
+++ commands/set-ticket.js
@@ -11,10 +11,13 @@ const { parseEmoji } = require('../utils/emoji');
 
 const PANEL_DESCRIPTION = [
     '# <a:lazo:1533270880673595402> 𝗍іᥴkᥱ𝗍s꒱꒱ ⑅ .ᐟ'
+    
     ,
     '𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟'
+    
     ,
     '⪩⪨    :Toca uno de los botones de  acuerdo a tu situation.ᐟ<:Nurse:1523126016057413763>'
+    
     ,
     '𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟𓎟'
 ].join('\n');
